ab29b9e202420cbf0dde644597329db9b6d7aedf
Abhinav Aryan <abhinav.aryan@aeologic.com>
2026-02-11 23:47:42 +0530

    Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCilrz0KbDzh3-7ru5T7ZGTZM8kZZQ_ms0",
-    authDomain: "ask-for-valentine.firebaseapp.com",
-    projectId: "ask-for-valentine",
-    storageBucket: "ask-for-valentine.firebasestorage.app",
-    messagingSenderId: "724715042005",
-    appId: "1:724715042005:web:a24f52223d34a7583b1e5c"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const storage = getStorage(app);
